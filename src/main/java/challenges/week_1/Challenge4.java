2728672d4a599b13deeb517ddb22b24c13ff8fbd
JamesDorling <jamesrdorling@gmail.com>
2021-12-04 22:35:22 +0000

    Tested challenge 4. Also made challenge 4 take an input (overloaded).

--- src/main/java/challenges/week_1/Challenge4.java
+++ src/main/java/challenges/week_1/Challenge4.java
@@ -27,4 +27,26 @@ public class Challenge4 {
         }
         return backName.equals(name.toLowerCase());
     }
+
+    public static boolean checkSingleNamePalindromic(String name)
+    {
+        //Initialise a string to add the characters to
+        String backName = "";
+        //Loop through each character
+        for(int i = 0; i < name.length(); i++)
+        {
+            //Add each character to a string backwards
+            backName += name.toLowerCase().charAt(name.length()-(i + 1)); //Concatenation in a loop is probably horrendous practice as strings are immutable.
+        }
+        //Use .equals (because == doesnt work with string) to compare the name backwards to the regular name, returning a boolean
+        if(backName.equals(name.toLowerCase()))
+        {
+            System.out.println("Name is palindromic.");
+        }
+        else
+        {
+            System.out.println("Name is not palindromic.");
+        }
+        return backName.equals(name.toLowerCase());
+    }
 }
--- src/test/java/challenges/week_1/Challenge4Tests.java
+++ src/test/java/challenges/week_1/Challenge4Tests.java
@@ -1,4 +1,30 @@
 package challenges.week_1;
 
+import org.junit.jupiter.api.Test;
+import  static org.junit.jupiter.api.Assertions.assertTrue;
+import  static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class Challenge4Tests {
+    Challenge4 fourthChall = new Challenge4();
+
+    @Test
+    public void palindromeTests()
+    {
+        //Does it give true for palindromic names and false for non-palindromic names?
+        assertTrue(fourthChall.checkSingleNamePalindromic("makam"));
+        assertFalse(fourthChall.checkSingleNamePalindromic("james"));
+    }
+
+    @Test
+    public void syntaxTest()
+    {
+        //Should work even with capitals
+        assertTrue(fourthChall.checkSingleNamePalindromic("Jamaj"));
+        //Test for special characters. No reason not to work, but worth checking.
+        assertTrue(fourthChall.checkSingleNamePalindromic("/'/&/'/"));
+        //This should technically return true, as nothing is still nothing backwards.
+        assertTrue(fourthChall.checkSingleNamePalindromic(""));
+        //This should say false because there are two names.
+        assertFalse(fourthChall.checkSingleNamePalindromic("jamaj dorlrod"));
+    }
 }
