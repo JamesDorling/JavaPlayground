1f2808960f5ca1377dfddcbb796ae9d11edf0cef
JamesDorling <jamesrdorling@gmail.com>
2021-12-05 13:22:10 +0000

    Tested fifth challenges and fixed issues they had

--- src/main/java/challenges/week_1/Challenge5.java
+++ src/main/java/challenges/week_1/Challenge5.java
@@ -3,49 +3,6 @@ package challenges.week_1;
 import java.util.ArrayList;
 
 public class Challenge5 {
-    public static ArrayList<String> reverseNameListArray(String names)
-    {
-        //This function is incorrect, as it uses string.split
-        String[] seperateNames = names.split(",");
-        ArrayList<String> finalNameList = new ArrayList<String>();
-        for(int i = 0; i < seperateNames.length; i++)
-        {
-                String[] individualNames = seperateNames[i].split(" ");
-                String reversedName = "";
-                for(int j = individualNames.length - 1; j >= 0; j--)
-                {
-                    reversedName += individualNames[j] + " ";
-                }
-                finalNameList.add(reversedName);
-        }
-
-        return finalNameList;
-    }
-
-    public static String reverseNameListString(String names)
-    {
-        //This function is incorrect, as it uses string.split
-        String[] seperateNames = names.split(",");
-        String reversedNameList = "";
-        for(int i = 0; i < seperateNames.length; i++)
-        {
-            String[] individualNames = seperateNames[i].split(" ");
-            String reversedName = "";
-            for(int j = individualNames.length - 1; j >= 0; j--)
-            {
-                reversedName += individualNames[j] + " ";
-            }
-            reversedNameList += reversedName + ", ";
-        }
-
-        StringBuffer finalNameListSB = new StringBuffer(reversedNameList);
-        while(finalNameListSB.charAt(finalNameListSB.length() - 1) == ',' || finalNameListSB.charAt(finalNameListSB.length() - 1) == ' ' ) {
-            finalNameListSB.deleteCharAt(finalNameListSB.length() - 1);
-        }
-
-        return finalNameListSB.toString();
-    }
-
     public static String reverseSingleNameSplitless(String name)
     {
         //The strings that the name will be put into
@@ -80,9 +37,65 @@ public class Challenge5 {
                 namesList.add(reverseSingleNameSplitless(name.substring(subStringMarker, i)));
                 //i + 2 to skip both the comma and the space.
                 subStringMarker = i + 2;
+            }//Else if to add final name without a ','. -1 because of indexing.
+            else if(i == name.length() - 1)
+            {
+                //Use "reverseSingleNameSplitless" to add the name to the arraylist already reversed
+                namesList.add(reverseSingleNameSplitless(name.substring(subStringMarker, i + 1)));
             }
         }
         //Return the arraylist as a string
-        return namesList.toString();
+        return convertArrayToString(namesList);
+    }
+
+    public static String convertArrayToString(ArrayList<String> listToConvert)
+    {
+        //This function is to seamlessly convert an arraylist to a string, so that it doesn't have a [] bracket at each end
+        String result = listToConvert.toString();
+        //Remove the end brackets
+        return result.substring(1, result.length() - 1);
+    }
+
+    public static ArrayList<String> reverseNameListArray(String names)
+    {
+        //This function is incorrect, as it uses string.split
+        String[] seperateNames = names.split(",");
+        ArrayList<String> finalNameList = new ArrayList<String>();
+        for(int i = 0; i < seperateNames.length; i++)
+        {
+            String[] individualNames = seperateNames[i].split(" ");
+            String reversedName = "";
+            for(int j = individualNames.length - 1; j >= 0; j--)
+            {
+                reversedName += individualNames[j] + " ";
+            }
+            finalNameList.add(reversedName);
+        }
+
+        return finalNameList;
+    }
+
+    public static String reverseNameListString(String names)
+    {
+        //This function is incorrect, as it uses string.split
+        String[] seperateNames = names.split(",");
+        String reversedNameList = "";
+        for(int i = 0; i < seperateNames.length; i++)
+        {
+            String[] individualNames = seperateNames[i].split(" ");
+            String reversedName = "";
+            for(int j = individualNames.length - 1; j >= 0; j--)
+            {
+                reversedName += individualNames[j] + " ";
+            }
+            reversedNameList += reversedName + ", ";
+        }
+
+        StringBuffer finalNameListSB = new StringBuffer(reversedNameList);
+        while(finalNameListSB.charAt(finalNameListSB.length() - 1) == ',' || finalNameListSB.charAt(finalNameListSB.length() - 1) == ' ' ) {
+            finalNameListSB.deleteCharAt(finalNameListSB.length() - 1);
+        }
+
+        return finalNameListSB.toString();
     }
 }
--- src/test/java/challenges/week_1/Challenge5Tests.java
+++ src/test/java/challenges/week_1/Challenge5Tests.java
@@ -1,5 +1,56 @@
 package challenges.week_1;
 
-public class Challenge5Tests {
+import org.junit.jupiter.api.Test;
 
+import  static org.junit.jupiter.api.Assertions.assertEquals;
+import  static org.junit.jupiter.api.Assertions.assertNotEquals;
+import  static org.junit.jupiter.api.Assertions.assertTrue;
+import  static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class Challenge5Tests {
+    Challenge5 fifthChall = new Challenge5();
+
+    /*
+    A couple functions in Challenge5 didnt fit the challenge. They are still there, but I see no reason to test them
+    as they are incorrect.
+    */
+
+    // Single Name Tests
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void singleNameTests()
+    {
+        // This should flip the name properly
+        assertTrue(fifthChall.reverseSingleNameSplitless("James Dorling").equals("Dorling James"));
+        // This is to check to see if it works but again, I suppose
+        assertFalse(fifthChall.reverseSingleNameSplitless("James Dorling").equals("James Dorling"));
+        // This should be true as the first name becomes "James Robert" as the function only tracks one space, and its the last one.
+        assertTrue(fifthChall.reverseSingleNameSplitless("James Robert Dorling").equals("Dorling James Robert"));
+        // This should be false because it should not flip just the first and last name, and therefore make the whole name go backwards.
+        assertFalse(fifthChall.reverseSingleNameSplitless("James Robert Dorling").equals("Dorling Robert James"));
+        // This should only move the final space to the back, as the function only tracks the final spacebar.
+        assertTrue(fifthChall.reverseSingleNameSplitless("James Robert Dorling ").equals(" James Robert Dorling"));
+    }
+
+    // List Name Tests
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void nameListTests()
+    {
+        //This function just calls the "reverseSingleNameSplitless" function on substrings created from a long string of names.
+
+        // This should flip the names properly
+        assertTrue(fifthChall.reverseNameListSplitless("James Dorling, Jordan Dorling, Alex Mace").equals("Dorling James, Dorling Jordan, Mace Alex"));
+        //ISSUE ONE this provides square brackets as it converts an arraylist to a string at the end
+        //ISSUE TWO this cuts out the final name because it does not have a comma at the end
+    }
+
+    @Test
+    public void nameListSyntaxTests()
+    {
+        //syntax tests, no reason not to pass but why not test it
+        assertTrue(fifthChall.reverseNameListSplitless("# 0, & =").equals("0 #, = &"));
+        //This should fail, as it will split the a and the d because of the comma.
+        assertFalse(fifthChall.reverseNameListSplitless("a,d ros").equals("ros a,d"));
+    }
 }
