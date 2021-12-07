86be5cd0ba67380f4f7ae4bcef927cd1341776e5
JamesDorling <jamesrdorling@gmail.com>
2021-12-07 17:12:14 +0000

    Completed the even fibonacci sum question.

--- src/test/java/challenges/EvenFibonacciSumTests.java
+++ src/test/java/challenges/EvenFibonacciSumTests.java
@@ -1,5 +1,6 @@
 package challenges;
 
+import core.array_functions.ArraySummer;
 import core.fibonacci.FibonacciCounter;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,10 @@ import  static org.junit.jupiter.api.Assertions.assertTrue;
 public class EvenFibonacciSumTests {
     //4 million for the ProjectEuler question
     FibonacciCounter fibCount = new FibonacciCounter(4000000);
+    //order is unecessary as is using the same function to calculate the fibonacci sequence as the "fibonacci counter".
     @Test
     public void EvenFibonacciTest()
     {
-        System.out.println();
+        assertEquals(9227464, ArraySummer.sumUpArray(fibCount.getSequence()));
     }
 }
