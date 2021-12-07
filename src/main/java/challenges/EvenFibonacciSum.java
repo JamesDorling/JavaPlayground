a509c3334e86488b5e8cbc72752c950bcb0b024b
JamesDorling <jamesrdorling@gmail.com>
2021-12-07 15:47:36 +0000

    Paused testing to make binarytree to specs

--- src/main/java/challenges/EvenFibonacciSum.java
+++ src/main/java/challenges/EvenFibonacciSum.java
@@ -1,32 +1,31 @@
 package challenges;
 
+import core.fibonacci.FibonacciCounter;
+
 import java.util.ArrayList;
 
 public class EvenFibonacciSum {
-    public static ArrayList<Integer> getFibonacciNumbers()
-    {
-        //Make the resulting integer and add the first two numbers
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(1);
-        result.add(1);
-        //Boolean for if it is higher than 4 million, as I don't need to go above that
-        boolean higherThan4Million = false;
-        //While we are dealing with numbers bellow 4 million
-        while (!higherThan4Million)
-        {
-            //Check if the number is higher than 4 million, if not then add the last two and loop again
-            if(result.get(-1) >= 4000000) { higherThan4Million = true; }
-            else {result.add((result.get(-1) + result.get(-2))); }
-        }
-        return removeOddNumbers(result);
-    }
-    /*
-    This is almost all we need, but I only need even numbers. The options are edit this to only take even numbers, or make another function.
-    Editing this would be the most efficient, and normally I would do this. However, making a new function is more work, and that is what I
-    am looking for.
-    */
-    public static ArrayList<Integer> removeOddNumbers(ArrayList<Integer> arrayOfInts) {
+    private FibonacciCounter fibCounter;
+    private ArrayList<Integer> fibSequence;
 
-        return null;
+    public EvenFibonacciSum(int limit) {
+        this.fibCounter = new FibonacciCounter(limit);
+        this.fibSequence = this.fibCounter.getSequence();
+        removeOddNumbers();
+    }
+
+    private void removeOddNumbers()
+    {
+        for(Integer i: fibSequence)
+        {
+            if(i % 2 == 0)
+            {
+                this.fibSequence.remove(i);
+            }
+        }
+    }
+
+    public ArrayList<Integer> getFibSequence() {
+        return fibSequence;
     }
 }
---
+++ src/test/java/challenges/EvenFibonacciSumTests.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import core.fibonacci.FibonacciCounter;
+import org.junit.jupiter.api.Test;
+
+import  static org.junit.jupiter.api.Assertions.assertEquals;
+import  static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import  static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EvenFibonacciSumTests {
+    @Test
+    public void EvenFibonacciTest()
+    {
+        //FibonacciCounter
+    }
+}
