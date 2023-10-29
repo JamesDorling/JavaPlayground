0f4a47ac2ef33e1df819c675f6052e50eae5b93d
JamesDorling <jamesrdorling@gmail.com>
2023-10-29 23:47:27 +0000

    made a decoder too

--- src/main/java/challenges/EdabitEncryption.java
+++ src/main/java/challenges/EdabitEncryption.java
@@ -12,11 +12,28 @@ public class EdabitEncryption {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < y; i++) {
             for (int j = i; j < message.length(); j+=y) {
-                // System.out.print(messageSplit[j]);
+                // System.out.print(message.charAt(j));
                 result.append(message.charAt(j));
             }
             // System.out.print("\n");
             result.append(" ");
+        }
+
+        return result.toString();
+    }
+
+    public static String decrypt(String message) {
+        if(message == null) return null;
+
+        String[] messageParts = message.split("[ ]");
+        StringBuilder result = new StringBuilder();
+        int loopTimes = (int)Math.ceil((double)message.replaceAll("[ ]", "").length() / messageParts.length); // Gotta be a better way to do this, looks nastyyyyy
+        for(int i = 0; i < loopTimes; i++) {
+            for(String part : messageParts) {
+                try {
+                    result.append(part.charAt(i));
+                } catch (StringIndexOutOfBoundsException ignored) {}
+            }
         }
 
         return result.toString();
@@ -28,5 +45,11 @@ public class EdabitEncryption {
         System.out.println(encrypt("feedthedog"));
         System.out.println(encrypt("chillout"));
         System.out.println(encrypt("A Fool and His Money Are Soon Parted."));
+
+        System.out.println(decrypt("imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"));
+        System.out.println(decrypt("hae and via ecy"));
+        System.out.println(decrypt("fto ehg ee dd"));
+        System.out.println(decrypt("clu hlt io"));
+        System.out.println(decrypt("Anoea FdnSr oHeot oiyoe lsAnd aMrP."));
     }
 }
