f7b0e61abceb591f90a816d8d1d158b52f128d39
JamesDorling <jamesrdorling@gmail.com>
2023-10-29 20:27:41 +0000

    finished edabit encryption

--- src/main/java/challenges/EdabitEncryption.java
+++ src/main/java/challenges/EdabitEncryption.java
@@ -3,15 +3,25 @@ package challenges;
 https://edabit.com/challenge/jfCsugyp9BSLYEtwb
  */
 public class EdabitEncryption {
-    public String encrypt(String message) {
-        int x = (int) Math.sqrt(message.length()); // This will truncate the double, therefore round down
-        int y = (int) Math.round(Math.sqrt(message.length())); // This will return a long. Just need to cast it.
-        char[][] characterMatrix = new char[x][y];
+    public static String encrypt(String message) {
+        if(message == null) return null;
 
-        return "AAAA";
+        int x = (int) Math.sqrt(message.length()); // This will truncate the double, therefore round down
+        int y = (int) Math.round(Math.sqrt(message.length())); // This will round up and return a long. Just need to cast it.
+        message = message.replaceAll("[ ]", "");
+        StringBuilder result = new StringBuilder();
+        String[] messageSplit = message.split("(?!^)");
+        for (int i = 0; i < x; i++) {
+            for (int j = i; j < message.length(); j+=y) {
+                result.append(messageSplit[j]);
+            }
+            result.append(" ");
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(encrypt("if man was meant to stay on the ground god would have given us roots"));
     }
 }
